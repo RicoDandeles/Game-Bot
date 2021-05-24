170162782e9a8445b0da07625e7123df257ff924
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-24 02:09:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on('messageReactionAdd', (reaction, user) => {
 		reaction.message.embeds.forEach((embed) => {
 			console.log("Scanning Embed");
 			var titleEmbed = embed.title;
-		}
+		});
 		console.log('Message: ' + titleEmbed);
 		console.log('Reaction: ' + reaction.emoji.name);
 		console.log('User: ' + user.id);
