d1e0c31e8a1b036f6604e15aea213168c292891a
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-25 17:19:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ function fetch_messages(searched_channel_id){
 			var keys = Array.from(messages.keys());
 			for ( var i=0; i < keys.length; i++){
 				var msg = client.channels.cache.get(game_log_channel).messages.fetch(keys[i])
+				console.log(msg.content);
 				if (msg.content.includes(searched_channel_id)){
 					console.log('success');
 					break;
