5be03db1fb87c47169adbad1dbba766f6db96c94
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-25 20:54:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -198,6 +198,7 @@ function fetch_messages(searched_channel_id){
 						else if (msgContent.includes(searched_channel_id)){
 							console.log('channel record found in logs');
 							return msg;
+							console.log('message returned');
 							status = 'success';
 						}	
 					});
