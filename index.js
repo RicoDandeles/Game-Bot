1239bfde46eda7ebe3f8371314c271916bc009da
RicoDandeles <83926161+RicoDandeles@users.noreply.github.com>
2021-05-25 21:47:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,6 @@ async function embedRelations(embedTitle, emojiName, userID, active_channel){
 	// 
 	var database_return = fetch_messages(active_channel);
 	if (database_return[0] != ''){
-		var database_return_id = channel.messages.fetch(database_return[1])
 		database_return = database_return[0];
 		// retrieve database information
 		channel = (database_return.split('|')[0]).split('Channel ID: ').join('').split(' ').join('');
@@ -184,8 +183,7 @@ async function embedRelations(embedTitle, emojiName, userID, active_channel){
 			  console.log('game name updated: ' + game_name);
   			  break;
 		}
-		console.log(database_return);
-		database_return_id.edit('Channel ID: ' + channel + ' | ' + 'Game Name: ' + game_name + ' | ' + 'Game Status: ' + game_status + ' | ' + 'Player 1: ' + player1 + ' | ' + 'Player 2: ' + player2 + ' | ' + 'Player 3: ' + player3 + ' | '  + 'Player 4: ' + player4 + ' | ' + 'Player 5: ' + player5 + ' | ' + 'Player 6: ' + player6 + ' | ' + 'Player 1 Bet: ' + player1_bet + ' | ' + 'Player 2 Bet: ' + player2_bet + ' | ' + 'Player 3 Bet: ' + player3_bet + ' | '  + 'Player 4 Bet: ' + player4_bet + ' | ' + 'Player 5 Bet: ' + player5_bet + ' | ' + 'Player 6 Bet: ' + player6_bet + ' | ')
+		database_return[1].edit('Channel ID: ' + channel + ' | ' + 'Game Name: ' + game_name + ' | ' + 'Game Status: ' + game_status + ' | ' + 'Player 1: ' + player1 + ' | ' + 'Player 2: ' + player2 + ' | ' + 'Player 3: ' + player3 + ' | '  + 'Player 4: ' + player4 + ' | ' + 'Player 5: ' + player5 + ' | ' + 'Player 6: ' + player6 + ' | ' + 'Player 1 Bet: ' + player1_bet + ' | ' + 'Player 2 Bet: ' + player2_bet + ' | ' + 'Player 3 Bet: ' + player3_bet + ' | '  + 'Player 4 Bet: ' + player4_bet + ' | ' + 'Player 5 Bet: ' + player5_bet + ' | ' + 'Player 6 Bet: ' + player6_bet + ' | ')
 		                 
 	}
 	else {
